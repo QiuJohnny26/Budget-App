1a99260cadce3fe91f491e12779deeb44a26f176
QiuJohnny26 <QiuJohnny26@hotmail.com>
2018-09-06 10:58:00 -0400

    Added Module Plans

---
+++ app.js
@@ -0,0 +1,19 @@
+// UI Controller
+var UIController=(function(){
+    
+})();
+
+//Budget Controller
+var BudgetController=(function(){
+    
+})();
+
+//Global Controller
+var GlobalController=(function(uiCTRL,budgetCTRL){
+    document.querySelector('.add__btn').addEventListener('click',function(){
+        //1.Get the filed input data
+    })
+})(UIController,BudgetController);
+
+
+
