86dd9053c8d65e1f8dee62bca9e994bdfbf5f80d
QiuJohnny26 <QiuJohnny26@hotmail.com>
2018-09-09 13:06:52 -0400

    UI/Getting Inputs

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 // UI Controller
 var UIController=(function(){
-    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
     return {
         getinput:function(){
-            var type = document.querySelector('.add__type').value; // will be either inc or exp selected
-            var description= document.querySelector('.add__description').value;
-            var value = document.querySelector('.add__value').value;
+            return{
+             type : document.querySelector(DOMstrings.inputType).value, // will be either inc or exp selected
+             description : document.querySelector(DOMstrings.inputDescription).value,
+             value : document.querySelector(DOMstrings.inputValue).value
+              };
+      },
+         getDOMstrings:function(){
+            return DOMstrings;
         }
-    }
+    };
+       
+    // if you return it , it will become public
 })();
 
 //Budget Controller
@@ -17,11 +29,13 @@ var BudgetController=(function(){
 
 //Global Controller
 var GlobalController=(function(uiCTRL,budgetCTRL){
-    
+    var DOM= uiCTRL.getDOMstrings();
     var addingItem= function(){
          //1.Get the filed input data
+        var input = uiCTRL.getinput();
+        console.log(input);
     }
-    document.querySelector('.add__btn').addEventListener('click',addingItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click',addingItem);
     document.addEventListener('keypress',function(event){
         if(event === 13 || event.which === 13){ //For Older browsers for event.which
             addingItem();
