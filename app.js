45af9d7cd39924f4f621a41700ef69cc4009c27c
QiuJohnny26 <QiuJohnny26@hotmail.com>
2018-09-24 12:31:17 -0400

    Fixed Budget Controller

--- app.js
+++ app.js
@@ -41,28 +41,43 @@ var BudgetController=(function(){
     
     var data = {
         allItems:{
-            exp: [],
-            inc: []
+            expense: [],
+            income: []
             
         },
         totals:{
-            exp: 0,
-            inc:0
+            expense: 0,
+            income: 0
         }
     
-    }
+    };
         return{
             addItem: function(type, des,val){
-            var newItem;
-            var ID = data.allItems[type][data.allItems[type].length-1]
-            if( type == 'exp'){
+            var newItem,ID;
+// Create new ID
+            if( data.allItems[type].length> 0){
+            ID=data.allItems[type][data.allItems[type].length-1].id+1;
+            }
+            else{
+            ID=0;
+            }
+ // Create new Item depending income or expense               
+            if( type === 'expense'){
             newItem = new Expense(ID,des,val);
             }
-            else if ( type == 'inc'){
-            newItem = new Income(ID,des,val)
+            else if ( type == 'income'){
+            newItem = new Income(ID,des,val);   
             }
-                data.allItems[type].push(newItem); //Adds Element to the end of the array
+//Adds Element to the end of the array
+             data.allItems[type].push(newItem);
+ //return new item
                 return newItem;
+            },
+            
+            testing: function() {
+                
+                console.log(data);
+            
             }
         };
 
@@ -84,12 +99,18 @@ var GlobalController=(function(uiCTRL,budgetCTRL){
     };
     var DOM= uiCTRL.getDOMstrings();
     var addingItem= function(){
-         //1.Get the filed input data
-        var input = uiCTRL.getinput();
+        var input,newItem;
+        //1.Get the filed input data
+         input = uiCTRL.getinput();
         console.log(input);
-    }
+         //2. Budget Controller
+       newItem = budgetCTRL.addItem(input.type,input.description,input.value);
+    };
+
+   
  return {
      init:function(){
+         console.log("starting");
         setupEventListeners();
      }
  };
