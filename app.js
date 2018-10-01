2136b201f2d630f476223d83f228fa9b5c954d36
QiuJohnny26 <QiuJohnny26@hotmail.com>
2018-10-01 09:54:39 -0400

    Inserted Adjacent HTML

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var UIController=(function(){
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        expensesExpenses:'.expenses__list',
+        incomeExpenses:'.income__list'
     };
     return {
         getinput:function(){
@@ -19,16 +21,21 @@ var UIController=(function(){
         },
         
         addListItems: function(obj, type){
-            var html,newHTML;
+            var html,newHTML,element;
             //create html string for placeholder
             if(type === 'income'){
+             element=DOMstrings.incomeExpenses;
              html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
             else if(type === 'expense')
-            html ='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%vale%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            element=DOMstrings.expensesExpenses;
+            html ='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             // replace the placeholder with some actual data
-            //newHTML =html.replace('id',obj.id)
+            newHTML = html.replace('%id%',obj.id);
+            newHTML = newHTML.replace('%description%',obj.description);
+            newHTML = newHTML.replace('%value%',obj.value);
             // Insert the HTML INTO THE DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend',newHTML);
             
         }
     };
@@ -83,7 +90,7 @@ var BudgetController=(function(){
             newItem = new Income(ID,des,val);   
             }
 //Adds Element to the end of the array
-             data.allItems[type].push(newItem);
+            data.allItems[type].push(newItem);
  //return new item
                 return newItem;
             },
@@ -119,6 +126,7 @@ var GlobalController=(function(uiCTRL,budgetCTRL){
         console.log(input);
          //2. Budget Controller
        newItem = budgetCTRL.addItem(input.type,input.description,input.value);
+       uiCTRL.addListItems(newItem,input.type);
     };
 
    
